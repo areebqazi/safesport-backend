dff7e8b78e33585d54f01d7253b75bd12f9757ad
webngigs <74106018+webngigs@users.noreply.github.com>
2024-10-21 23:03:20 +0530

    Update userRouter.js
    Email body and subject changes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -89,8 +89,20 @@ router.post("/send-certificate", async (req, res) => {
       // from: process.env.GODADDY_EMAIL, // Sender address
       from: "hello@safesportforyouth.org",
       to: email,
-      subject: "Your Certificate",
-      html: `<p>Attached is your certificate.</p>`,
+      subject: "Your Safe Sport Education for Youth Completion Certificate",
+      html: `<img src="https://www.safesportforyouth.org/static/media/safesportlogo.7257988d1679cec6cdbd.png"><br /><br /><p><b>Thank You!</b></p> <br />
+        <p>Thank you for completing the Safe Sport Education for Youth program!</p>
+        <p>Attached you will find your certificate of completion that can be submitted to your club as proof of completion, if required. You can also log into your account at <a href="https://safesportforyouth.org">safesportforyouth.org</a> to download your certificate.</p>
+        <p>Additional Safe Sport resources are available at <a href="https://www.safesportforyouth.org/Resources">this link</a> and the videos will remain online for you to re-watch at any time by logging into your account.</p>
+        <p>If you have any questions about the program please visit our Frequently Asked Questions page or contact us at <a href="mailto:hello@safesportforyouth.org">hello@safesportforyouth.org</a>.</p> <br /> <br /> <br />
+        
+        <p>------------------------------------------------</p> <br /><br /><br />
+        
+        <p><b>Merci!</b></p>
+        <p>Merci d'avoir suivi le programme de formation au sport sécuritaire pour les jeunes !</p>
+        <p>Vous trouverez ci-joint votre certificat de réussite que vous pourrez présenter à votre club ou à votre organisme provincial de sport comme preuve de réussite, le cas échéant. Vous pouvez également vous connecter à votre compte sur <a href="https://safesportforyouth.org">safesportforyouth.org</a> pour télécharger votre certificat.</p>
+        <p>D'autres ressources sur le sport sécuritaire sont disponibles à <a href="https://www.safesportforyouth.org/Resources">cette page</a>. Les vidéos demeurent en ligne et vous pouvez les visionner à tout moment en vous connectant à votre compte.</p>
+        <p>Si vous avez des questions sur le programme, visitez notre page de questions fréquemment posées ou contactez-nous à <a href="mailto:hello@safesportforyouth.org">hello@safesportforyouth.org</a>.</p>`,
       attachments: [
         {
           filename: "certificate.png",
